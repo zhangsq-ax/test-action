5a45379c166ac970eadd6b35352def235bef51c5
zhangsq <zhangsq@mapbar.com>
2020-10-21 12:04:39 +0800

    feat: Supported custom names

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -8,12 +9,13 @@ import (
 func main() {
 	http.HandleFunc("/", index)
 
-	log.Println("Start server...")
+	log.Println("Start server at port 8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func index(req http.ResponseWriter, res *http.Request) {
-	_, err := req.Write([]byte("Hello World!"))
+func index(res http.ResponseWriter, req *http.Request) {
+	_, err := fmt.Fprintf(res,"Hello, %s", req.URL.Path)
+	//_, err := res.Write([]byte("Hello World!"))
 	if err != nil {
 		log.Println(err)
 	}
